c01deafa9162125a6e24abd278673b84a5d3db6c
Md Minhazur Rahman <rafi.rahman03@yahoo.com>
2022-08-18 13:50:50 +0600

    url disabled

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import {CssBaseline, ThemeProvider} from "@mui/material";
 import {theme} from "./theme";
 import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 
-const client = new ApolloClient({
-    uri: 'http://localhost:5000/graphql',
-    cache: new InMemoryCache()
-});
+// const client = new ApolloClient({
+//     uri: 'http://localhost:5000/graphql',
+//     cache: new InMemoryCache()
+// });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
